a4797c7015959727da57c2223e5c425b3f3d91d4
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-20 14:20:19 -0300

    Colocar os produtos na lista

--- js/app.js
+++ js/app.js
@@ -2,15 +2,12 @@ function add(){
     let product = document.getElementById('product').value;
     let nameProcuct = product.split('-')[0];
     let valueProduct = product.split('R$')[1];
+    let quantity = document.getElementById('quantity').value;  
 
-    let quantity = document.getElementById('quantity');
-
-    alert(product);
-    alert(quantity.value);
-    alert(nameProcuct);
-    alert(valueProduct);
-
-    price(valueProduct, quantity);
+    let listProducts = document.getElementById('lista-produtos');
+    listProducts.innerHTML = listProducts.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantity}x</span> ${nameProcuct} <span class="texto-azul">R$${totalPrice (valueProduct, quantity)}</span>
+        </section>`;
 }
 
 function clean(){
@@ -18,7 +15,7 @@ function clean(){
 }
 
 function totalPrice (valueProduct, quantity){
-    let price = valueProduct * quantity.value;
+    let price = valueProduct * quantity;
 
-    alert(`Preço total R$ ${price}`);
+    return price;
 }
