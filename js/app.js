6ca179eeed571a5951131866648cef6610d14c10
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-20 10:03:09 -0300

    Pegar as informações do produto

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function add(){
+    let product = document.getElementById('product');
+    let quantity = document.getElementById('quantity');
+
+    alert(product.value);
+    alert(quantity.value);
+}
+
+function clean(){
+
+}
