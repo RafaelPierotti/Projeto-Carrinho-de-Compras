178990f465b83f5992b87de77783fd44e27f4d9a
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-20 10:26:21 -0300

    Calcular preço total

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 function add(){
-    let product = document.getElementById('product');
+    let product = document.getElementById('product').value;
+    let nameProcuct = product.split('-')[0];
+    let valueProduct = product.split('R$')[1];
+
     let quantity = document.getElementById('quantity');
 
-    alert(product.value);
+    alert(product);
     alert(quantity.value);
+    alert(nameProcuct);
+    alert(valueProduct);
+
+    price(valueProduct, quantity);
 }
 
 function clean(){
 
+}
+
+function totalPrice (valueProduct, quantity){
+    let price = valueProduct * quantity.value;
+
+    alert(`Preço total R$ ${price}`);
 }
